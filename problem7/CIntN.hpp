b5c9131eb4f3a15e9ce1542789ff4d04cac8a33e
Александр <axew165@gmail.com>
2021-02-17 23:22:35 +0300

    Solve problem 7

---
+++ problem7/CIntN.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <exception>
+
+//constexpr size_t N = 29;
+
+class CIntN {
+public:
+	uint32_t* number;
+	size_t size;
+	size_t N;
+public:
+	CIntN(size_t);
+	CIntN(size_t, long long);
+	CIntN(size_t, int);
+	CIntN(size_t, const char*);
+	CIntN(const CIntN&);
+	CIntN(CIntN&&) noexcept;
+	~CIntN();
+	friend std::ostream& operator<<(std::ostream&, const CIntN&);
+	CIntN& operator+=(const CIntN&);
+	CIntN& operator+(const CIntN& right);
+	CIntN& operator-=(const CIntN&);
+	CIntN& operator-(const CIntN& right);
+	CIntN& operator=(const CIntN&);
+	CIntN& operator=(CIntN&&) noexcept;
+	bool operator==(const CIntN& right) const;
+	bool operator!=(const CIntN& right) const;
+
+	virtual int output(const char* FileName = nullptr) = 0;
+};
+
+class CIntN0 : public CIntN {
+public:
+	using CIntN::CIntN;
+	int output(const char* FileName = NULL) override {
+		if (FileName == nullptr) {
+			std::cout << "Error occured while working with file!" << std::endl;
+			return 1;
+		}
+		std::ofstream out(FileName,std::ios::app);
+		out << *this << std::endl;
+		out.close();
+		return 0;
+	}
+};
+
+class CIntN1 : public CIntN {
+public:
+	using CIntN::CIntN;
+	int output(const char* FileName = NULL) override {
+		std::cout << this->N << std::endl << *this;
+		return 0;
+	}
+};
+
+class Creator {
+public:
+	virtual CIntN* create(size_t size, const char* num)=0;
+};
+
+class CreatorIntN0 : public Creator {
+public:
+	CIntN* create(size_t size, const char* num) override {
+		return new CIntN0(size, num);
+	}
+};
+
+class CreatorIntN1 : public Creator {
+public:
+	CIntN* create(size_t size, const char* num) override {
+		return new CIntN1(size, num);
+	}
+};
---
+++ problem7/main.cpp
@@ -0,0 +1,34 @@
+#include "CIntN.hpp"
+#include "AutoTest.hpp"
+#include <string>
+using namespace std;
+
+int main() {
+	if (AutoTest()) {
+		cout << "AutoTest not complete!" << endl;
+		return 0;
+	}
+	cout << "AutoTest complete!" << endl;
+	Creator* create[2] = {new CreatorIntN0(), new CreatorIntN1()};
+	ifstream in("input.txt");
+	int n = 0;
+	in >> n;
+	CIntN** arr = new CIntN* [n];
+	int j = 0;
+	int s = 0;
+	std::string num;
+	for (int i = 0; i < n; i++) {
+		in >> j;
+		in >> s;
+		in >> num;
+		arr[i] = create[j]->create(s, num.c_str());
+	}
+	for (int i = 0; i < n; i++) {
+		arr[i]->output("out.txt");
+	}
+	for (int i = 0; i < n; i++) {
+		delete arr[i];
+	}
+	delete[] arr;
+	return 0;
+}
