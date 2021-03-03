04a47b59d008ca61194471faf67f6e69cb16f8e3
Александр <axew165@gmail.com>
2021-03-03 23:55:20 +0300

    Fix according to pt 4 of problem formulation

--- problem8/main.cpp
+++ problem8/main.cpp
@@ -1,6 +1,7 @@
 #include "CIntN.hpp"
 #include "AutoTest.hpp"
 #include <map>
+#include <utility>
 using namespace std;
 
 int main() {
@@ -20,14 +21,19 @@ int main() {
 	std::string j;
 	int s = 0;
 	std::string num;
-	for (int i = 0; i < n; i++) {
+	for (auto i = arr.begin(); i != arr.end(); i++) {
 		in >> j;
 		in >> s;
 		in >> num;
-		arr[i] = create[j]->create(s, num.c_str());
+		for (const auto& k : create) {
+			if (k.first == j) {
+				*i = (k.second)->create(s, num.c_str());
+				break;
+			}
+		}
 	}
-	for (int i = 0; i < n; i++) {
-		arr[i]->output("out.txt");
+	for (auto i = arr.begin(); i != arr.end(); i++) {
+		(*i)->output("out.txt");
 	}
 	arr.clear();
 	create.clear();
