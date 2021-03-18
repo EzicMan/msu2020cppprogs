e107ed8fdfce546344cc6f113b2fb564caebf38b
Александр <axew165@gmail.com>
2021-03-18 12:15:34 +0300

    Sry my mistake now omp just works

--- problem9/AutoTest.hpp
+++ problem9/AutoTest.hpp
@@ -29,21 +29,21 @@ bool AutoTest() {
     end = std::chrono::system_clock::now();
     elapsed_ms = static_cast<int>( std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() );
     std::cout << "Many subtractions operator runtime is " << elapsed_ms << " ms\n";
-	start = std::chrono::system_clock::now();
-	
 	long long num_dimensions = 10e7;
 
 	std::string digits_1, digits_2;
 	for (int i = 0; i < num_dimensions; ++i) // Gens rnadom numbers
 	{
-		digits_1.push_back(rand() % 10+'0');
-		digits_2.push_back(rand() % 10+'0');
+		digits_1.push_back(rand() % 10 + '0');
+		digits_2.push_back(rand() % 10 + '0');
 	}
+
+	start = std::chrono::system_clock::now();
 
 	CIntN0 num_1(num_dimensions, digits_1.c_str(),false);
 	CIntN0 num_2(num_dimensions, digits_1.c_str(),false);
 
-	CIntN0 tmp = num_1 + num_2;
+	CIntN0 tmpplus = num_1 + num_2;
 	
 	end = std::chrono::system_clock::now();
 	elapsed_ms = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
@@ -51,18 +51,10 @@ bool AutoTest() {
 
 	start = std::chrono::system_clock::now();
 
-	tmp = num_1 - num_2;
-
-	end = std::chrono::system_clock::now();
-	elapsed_ms = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
-	std::cout << "One big substraction without omp operator runtime is " << elapsed_ms << " ms\n";
-
-	start = std::chrono::system_clock::now();
-
 	CIntN0 num_11(num_dimensions, digits_1.c_str(),true);
 	CIntN0 num_21(num_dimensions, digits_1.c_str(),true);
 
-	CIntN0 tmp1 = num_11 + num_21;
+	CIntN0 tmp1plus = num_11 + num_21;
 
 	end = std::chrono::system_clock::now();
 	elapsed_ms = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
@@ -70,10 +62,21 @@ bool AutoTest() {
 
 	start = std::chrono::system_clock::now();
 
-	tmp1 = num_11 - num_21;
+	CIntN0 tmpminus = num_1 - num_2;
+
+	end = std::chrono::system_clock::now();
+	elapsed_ms = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+	std::cout << "One big substraction without omp operator runtime is " << elapsed_ms << " ms\n";
+
+	start = std::chrono::system_clock::now();
+
+	CIntN0 tmp1minus = num_11 - num_21;
 
 	end = std::chrono::system_clock::now();
 	elapsed_ms = static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
 	std::cout << "One big substraction with omp operator runtime is " << elapsed_ms << " ms\n";
+	if (tmp1plus != tmpplus && tmp1minus != tmpminus) {
+		return false;
+	}
 	return true;
 }
